3aa9eac3e134d2594b80eb65bb664c70cb46292a
lukaszGancarz <lukasz.gancarz@wp.pl>
2021-10-24 16:18:14 +0200

    circle added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import rough from "roughjs/bundled/rough.esm";
 const generator = rough.generator();
 
 function createElement(x1, y1, x2, y2, type) {
-  const roughElement =
-    type === "line"
-      ? generator.line(x1, y1, x2, y2)
-      : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
+  let roughElement = generator.line(x1, y1, x2, y2);
+  if (type === "rectangle")
+    roughElement = generator.rectangle(x1, y1, x2 - x1, y2 - y1);
+  if (type === "circle") roughElement = generator.circle(x1, y1, (x2 - x1) * 2);
   return { x1, y1, x2, y2, roughElement };
 }
 
@@ -74,6 +74,13 @@ function App() {
           onChange={() => setElementType("rectangle")}
         />
         <label htmlFor="rectangle">Rectangle</label>
+        <input
+          type="radio"
+          id="circle"
+          checked={elementType === "circle"}
+          onChange={() => setElementType("circle")}
+        />
+        <label htmlFor="circle">Circle</label>
       </div>
       <canvas
         id={"canvas"}
